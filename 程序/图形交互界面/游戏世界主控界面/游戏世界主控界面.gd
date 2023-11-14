7236bb8a74cc068cfec3b0c1a40178973e17cab4
daheilizi <1113241799@qq.com>
2023-11-14 15:24:32 +0800

    场景结构更改 	游戏世界坐标层 	游戏世界事物层 初步调整角色场景

--- 程序/图形交互界面/游戏世界主控界面/游戏世界主控界面.gd
+++ 程序/图形交互界面/游戏世界主控界面/游戏世界主控界面.gd
@@ -2,7 +2,8 @@ extends Control
 class_name 游戏世界主控界面
 
 
-@onready var 消息面板 = %消息面板
+@onready var 先攻顺序 = %先攻顺序
+@onready var 全部 = %全部
 
 func _ready() -> void:
 	# 连接信号 Global._消息系统.信号_发送消息
@@ -12,5 +13,5 @@ func _ready() -> void:
 
 func 触发_发送消息(para_消息类型: Global.消息类型, para_消息内容: String) -> void:
 	# 为’全部‘添加消息内容
-	消息面板.get_node("全部").add_text(para_消息内容)
+	全部.add_text(para_消息内容)
 	pass
